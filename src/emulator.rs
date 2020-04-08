c650be49c48b82028a380eff9b5c7d712baae80b
dramikei <raghav2011rv@gmail.com>
2020-04-09 02:31:14 +0530

    Wrote ORI SUI ORA LHLD RAR RLC instructions

--- src/emulator.rs
+++ src/emulator.rs
@@ -337,6 +337,36 @@ impl Emulator {
 
     }
 
+     //Carry is reset to zero. Flags affected: Carry, Zero, Sign, Parity.
+     fn ora(cpu: &mut cpu::CPU, reg: Reg) {
+        let result: u16;
+        let operand: u8;
+        cpu.flags.cy = false;
+        match reg {
+            Reg::B => operand = cpu.b,
+            Reg::C => operand = cpu.c,
+            Reg::D => operand = cpu.d,
+            Reg::E => operand = cpu.e,
+            Reg::H => operand = cpu.h,
+            Reg::L => operand = cpu.l,
+
+            Reg::HL => {
+                let addr = (((cpu.h as u16) << 8) | (cpu.l as u16)) as usize;
+                operand = cpu.memory[addr];
+            },
+
+            Reg::A => operand = cpu.a,
+            _ => {
+                panic!("ORA Called on wrong REG!");
+            }
+        }
+        result = cpu.a as u16 | operand as u16;
+        //TODO: Check if Aux Carry is affected or not.
+        cpu.flags.set_all_but_aux_carry(result);
+        cpu.a = result as u8;
+
+    }
+
     fn dad(cpu: &mut cpu::CPU, reg: Reg) {
         let result: u32;
         match reg {
@@ -502,6 +532,12 @@ impl Emulator {
             0x03 => Self::inx(cpu, Reg::B),
             0x05 => Self::dcr(cpu, Reg::B),
             0x06 => Self::mvi(cpu, Reg::B),
+            0x07 => {
+                let bit7: u8 = cpu.a & (1 << 7);
+                cpu.a <<= 1;
+                cpu.a |= bit7 >> 7;
+                cpu.flags.cy = bit7 != 0;
+            }
             0x09 => Self::dad(cpu, Reg::B),
             0x0a => Self::ldax(cpu, Reg::B),
             0x0d => Self::dcr(cpu, Reg::C),
@@ -520,11 +556,23 @@ impl Emulator {
             0x1a => Self::ldax(cpu, Reg::D),
             0x1d => Self::dcr(cpu, Reg::E),
             0x1e => Self::mvi(cpu, Reg::E),
+            0x1f => {
+                let bit0: u8 = cpu.a & 1;
+                cpu.a >>= 1;
+                if cpu.flags.cy { cpu.a |= 1 << 7; }
+                cpu.flags.cy = bit0 != 0;
+            }
             0x21 => Self::lxi(cpu, Reg::H),
             0x23 => Self::inx(cpu, Reg::H),
             0x25 => Self::dcr(cpu, Reg::H),
             0x26 => Self::mvi(cpu, Reg::H),
             0x29 => Self::dad(cpu, Reg::H),
+            0x2a => {
+                let addr = ((cpu.memory[(cpu.pc as usize) + 2] as u16) << 8) | cpu.memory[(cpu.pc as usize) + 1] as u16;
+                cpu.l = cpu.memory[addr as usize];
+                cpu.h = cpu.memory[addr as usize + 1];
+                cpu.pc = cpu.pc.wrapping_add(2);
+            }
             0x2d => Self::dcr(cpu, Reg::L),
             0x2e => Self::mvi(cpu, Reg::L),
             0x31 => Self::lxi(cpu, Reg::SP),
@@ -567,16 +615,13 @@ impl Emulator {
             0x98 ..= 0x9f => Self::sub(cpu, Self::extract_argument(opcode), cpu.flags.cy),
             0xA0 ..= 0xA7 => Self::ana(cpu, Self::extract_argument(opcode)),
             0xA8 ..= 0xAf => Self::xra(cpu, Self::extract_argument(opcode)),
+            0xb0 ..= 0xb7 => Self::ora(cpu, Self::extract_argument(opcode)),
 
+            0xc0 => if !cpu.flags.z { Self::ret(cpu) },
             0xc1 => Self::pop(cpu, Reg::B),
-            0xc2 => {
-                if !cpu.flags.z {
-                    Self::jmp(cpu);
-                } else {
-                    cpu.pc = cpu.pc.wrapping_add(2);
-                }
-            }
+            0xc2 => if !cpu.flags.z { Self::jmp(cpu) } else { cpu.pc = cpu.pc.wrapping_add(2) },
             0xc3 => Self::jmp(cpu),
+            0xc4 => if !cpu.flags.z { Self::call(cpu) } else { cpu.pc = cpu.pc.wrapping_add(2) },
             0xc5 => Self::push(cpu, Reg::B),
             0xc6 => {
                 let data = cpu.memory[(cpu.pc as usize) + 1];
@@ -587,13 +632,22 @@ impl Emulator {
             },
             0xc8 => if cpu.flags.z { Self::ret(cpu) },
             0xc9 => Self::ret(cpu),
-            0xca => if cpu.flags.z { Self::jmp(cpu) } else { cpu.pc = cpu.pc.wrapping_add(2) }
+            0xca => if cpu.flags.z { Self::jmp(cpu) } else { cpu.pc = cpu.pc.wrapping_add(2) },
+            0xcc => if cpu.flags.z { Self::call(cpu) } else { cpu.pc = cpu.pc.wrapping_add(2) },
             0xcd => Self::call(cpu),
 
+            0xd0 => if !cpu.flags.cy { Self::ret(cpu) },
             0xd1 => Self::pop(cpu, Reg::D),
             0xd2 => if !cpu.flags.cy { Self::jmp(cpu) } else { cpu.pc = cpu.pc.wrapping_add(2) }
             0xd3 => Self::out(cpu),
             0xd5 => Self::push(cpu, Reg::D),
+            0xd6 => {
+                let data = cpu.memory[(cpu.pc as usize) + 1];
+                let result = (cpu.a as u16).wrapping_sub( data as u16);
+                cpu.flags.set_all(result, (cpu.a & 0xf).wrapping_sub(data & 0xf));
+                cpu.a = result as u8;
+                cpu.pc = cpu.pc.wrapping_add(1);
+            }
             0xd8 => if cpu.flags.cy { Self::ret(cpu) },
             0xda => if cpu.flags.cy { Self::jmp(cpu) } else { cpu.pc = cpu.pc.wrapping_add(2) },
             0xdb => Self::emu_in(cpu),
@@ -618,6 +672,11 @@ impl Emulator {
 
             0xf1 => Self::pop_psw(cpu),
             0xf5 => Self::push_psw(cpu),
+            0xf6 => {
+                cpu.a |= cpu.memory[cpu.pc as usize + 1];
+                cpu.flags.set_all_but_aux_carry(cpu.a as u16);
+                cpu.pc = cpu.pc.wrapping_add(1);
+            },
             0xfb => cpu.interrupts_enabled = true,
             0xfe => {
                 let operand = cpu.memory[(cpu.pc as usize) + 1];
